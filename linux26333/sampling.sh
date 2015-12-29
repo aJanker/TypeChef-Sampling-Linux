e6027c873e7efdf1cf3fb5677498ec9e69a258d8
Andreas Janker <janker@ravel.fim.uni-passau.de>
2015-12-29 17:10:50 +0100

    updated sampling script

--- linux26333/sampling.sh
+++ linux26333/sampling.sh
@@ -16,20 +16,33 @@ copyBase=$(echo $outBase | sed s/local/scratch/g)
     ./jcpp_sampling.sh "$@" $extraFlagsSC
     gzip ${outBase}.err ${copyBase}_singleconf.err.gz
     gzip ${outBase}.dbg ${copyBase}_singleconf.dbg.gz
+    mv ${outBase}.time ${copyBase}_singleconf.time
+    mv ${outBase}.astimes ${copyBase}_singleconf.astimes
+    mv ${outBase}.errreport ${outBase}_singleconf.errreport	
 
  # pairwise
     extraFlagsPW="--pairwise --rootfolder /loccal/janker/linuxMax"
     ./jcpp_sampling.sh "$@" $extraFlagsPW
     gzip ${outBase}.err ${copyBase}_pairwise.err.gz
     gzip ${outBase}.dbg ${copyBase}_pairwise.dbg.gz
+    mv ${outBase}.time ${copyBase}_pairwise.time
+    mv ${outBase}.astimes ${copyBase}_pairwise.astimes
+    mv ${outBase}.errreport ${outBase}_pairwise.errreport 
 
  # code coverage nh
     extraFlagsCC="--rootfolder /loccal/janker/linuxMax --codecoveragenh"
     ./jcpp_sampling.sh "$@" $extraFlagsCC
     gzip ${outBase}.err ${copyBase}_codecoveragenh.err.gz
     gzip ${outBase}.dbg ${copyBase}_codecoveragenh.dbg.gz
+    mv ${outBase}.time ${copyBase}_codecoveragenh.time
+    mv ${outBase}.astimes ${copyBase}_codecoveragenh.astimes
+    mv ${outBase}.errreport ${outBase}_codecoveragenh.errreport 	
 
 # create condensed report
     find ${outBase} -type f -name "${filename}.c_*.errreport" | xargs cat > ${copyBase}.errreportall
+
+    gzip ${outBase}_codecoveragenh.errreport ${copyBase}_codecoveragenh.errreport.gz
+    gzip ${outBase}_pairwise.errreport ${copyBase}_pairwise.errreport.gz
+    gzip ${outBase}_singleconf.errreport ${copyBase}_singleconf.errreport.gz
 
 #mv to scratch
